b482537f9f60291d31915929323e732488a5127e
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-10 10:35:38 +0200

    Playlists module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'playlists', loadChildren: () => import('./playlists/playlists.module').then(m => m.PlaylistsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
