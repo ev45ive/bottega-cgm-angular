3c29220a0667400a691daac345e93002f99ada6d
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-10 15:10:12 +0200

    <ng-template ngFor  [ngForOf]="playlists" let-playlist="$implicit" let-i="index"

--- src/app/playlists/components/playlist-list/playlist-list.component.ts
+++ src/app/playlists/components/playlist-list/playlist-list.component.ts
@@ -1,10 +1,33 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
+NgFor
 
 @Component({
   selector: 'app-playlist-list',
   templateUrl: './playlist-list.component.html',
-  styleUrl: './playlist-list.component.scss'
+  styleUrl: './playlist-list.component.scss',
 })
 export class PlaylistListComponent {
-
+  
+  playlists = [
+    {
+      id: '123',
+      name: 'Playlist 123',
+      public: true,
+      description: 'Awesome playlsit',
+    },
+    {
+      id: '234',
+      name: 'Playlist 234',
+      public: false,
+      description: 'Best playlsit',
+    },
+    {
+      id: '345',
+      name: 'Playlist 345',
+      public: true,
+      description: 'Cool playlsit',
+    },
+  ];
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClockComponent } from './components/clock/clock.component';
 import { FormsModule } from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
+const MaterialImports = [
+  MatButtonModule,
+  MatListModule
+]
 
 @NgModule({
   declarations: [
@@ -11,13 +17,13 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [
     CommonModule,
     FormsModule,
-    MatButtonModule
+    MaterialImports 
   ],
   exports:[
     ClockComponent,
     CommonModule,
     FormsModule,
-    MatButtonModule
+    MaterialImports
   ]
 })
 export class SharedModule { }
