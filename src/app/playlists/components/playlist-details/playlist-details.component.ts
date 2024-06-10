9e4a848435b9cf63983e2ef0093662a2ba4572ec
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-10 11:32:50 +0200

    :host ::ng-deep  -  :host selector - interface,  CSS - implementation

--- src/app/playlists/components/playlist-details/playlist-details.component.ts
+++ src/app/playlists/components/playlist-details/playlist-details.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-playlist-details',
   templateUrl: './playlist-details.component.html',
   styleUrl: './playlist-details.component.scss',
+  // encapsulation: ViewEncapsulation.None
 })
 export class PlaylistDetailsComponent {
   playlist = {
