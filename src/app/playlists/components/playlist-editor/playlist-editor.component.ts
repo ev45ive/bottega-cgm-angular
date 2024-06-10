dab501070904539666fcb9fc72c4b0c4a8add906
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-10 12:18:50 +0200

    OnPush, ngDoCheck, ChangeDetectorRef, cdr.detectChanges, cdr.markForCheck

--- src/app/playlists/components/playlist-editor/playlist-editor.component.ts
+++ src/app/playlists/components/playlist-editor/playlist-editor.component.ts
@@ -1,12 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
 
 @Component({
   selector: 'app-playlist-editor',
   templateUrl: './playlist-editor.component.html',
   styleUrl: './playlist-editor.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush // d|-_-|b
+  changeDetection: ChangeDetectionStrategy.OnPush, // d|-_-|b
 })
 export class PlaylistEditorComponent {
+  // @Input() playlist: Class;
+
+  ngDoCheck(): void { // parent is being checked
+    this.cdr.markForCheck() // detect once with parent
+  }
+
+  constructor(private cdr: ChangeDetectorRef) {
+    this.cdr.detectChanges();
+  }
 
   playlist = {
     id: '123',
