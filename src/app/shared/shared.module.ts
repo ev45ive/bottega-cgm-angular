1c0143e491e21ec30aad2d4a5c2444d70e0e4aa6
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-10 15:40:12 +0200

    mat-form-field + matInput -> input[ngModel]

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,14 @@ import { ClockComponent } from './components/clock/clock.component';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const MaterialImports = [
   MatButtonModule,
-  MatListModule
+  MatListModule,
+  MatFormField,
+  MatInputModule,
 ]
 
 @NgModule({
