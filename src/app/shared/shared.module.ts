5e4ef8d892aaae5bf627a25df66fefbcf82d3ab4
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-10 14:10:04 +0200

    Switching modes + mat buttons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClockComponent } from './components/clock/clock.component';
 import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,11 +11,13 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     FormsModule,
+    MatButtonModule
   ],
   exports:[
     ClockComponent,
     CommonModule,
     FormsModule,
+    MatButtonModule
   ]
 })
 export class SharedModule { }
