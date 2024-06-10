260c8d05fe8a1bcc219acde10e72414d306ec990
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-10 13:40:03 +0200

    [value]="playlist.name" (input)="playlist.name = $any($event.target).value"

--- src/app/shared/components/clock/clock.component.ts
+++ src/app/shared/components/clock/clock.component.ts
@@ -19,10 +19,10 @@ export class ClockComponent {
   time = ''; //new Date().toLocaleTimeString();
 
   constructor(
-      private app: ApplicationRef,
-      private cdr: ChangeDetectorRef,
-      private zone: NgZone,
-      @Inject(PLATFORM_ID) private pid: object
+    private app: ApplicationRef,
+    private cdr: ChangeDetectorRef,
+    private zone: NgZone,
+    @Inject(PLATFORM_ID) private pid: object
   ) {}
 
   ngOnInit(): void {
@@ -30,9 +30,7 @@ export class ClockComponent {
       this.zone.runOutsideAngular(() => {
         setInterval(() => {
           this.time = new Date().toLocaleTimeString();
-          // $timeout, $scope.apply() $scope.diget() cdr.detectChagnes()
-          // this.app.tick(); // auto - zoneJS monkey patch
-          this.cdr.detectChanges() // only me!
+          this.cdr.detectChanges();
         }, 1000);
       });
   }
