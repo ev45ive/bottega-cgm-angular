0cdd5d7de0757a8d65ac04d0e156c67129461fb6
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-11 14:10:38 +0200

    @ - at syntax  + prettier

--- src/app/music/components/results-grid/results-grid.component.ts
+++ src/app/music/components/results-grid/results-grid.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AlbumCardComponent } from "../album-card/album-card.component";
 
 @Component({
-  selector: 'app-results-grid',
-  standalone: true,
-  imports: [],
-  templateUrl: './results-grid.component.html',
-  styleUrl: './results-grid.component.scss'
+    selector: 'app-results-grid',
+    standalone: true,
+    templateUrl: './results-grid.component.html',
+    styleUrl: './results-grid.component.scss',
+    imports: [AlbumCardComponent]
 })
 export class ResultsGridComponent {
 
