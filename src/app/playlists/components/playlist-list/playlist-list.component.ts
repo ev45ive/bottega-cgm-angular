929d73c8a46d070d67d8bdb728f8bab8a506aa43
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-11 09:23:58 +0200

    Selecting playlists

--- src/app/playlists/components/playlist-list/playlist-list.component.ts
+++ src/app/playlists/components/playlist-list/playlist-list.component.ts
@@ -3,32 +3,39 @@ import { Component } from '@angular/core';
 
 NgFor;
 
+const mockPlaylists = [
+  {
+    id: '123',
+    name: 'Playlist 123',
+    public: true,
+    description: 'Awesome playlsit',
+  },
+  {
+    id: '234',
+    name: 'Playlist 234',
+    public: false,
+    description: 'Best playlsit',
+  },
+  {
+    id: '345',
+    name: 'Playlist 345',
+    public: true,
+    description: 'Cool playlsit',
+  },
+];
 @Component({
   selector: 'app-playlist-list',
   templateUrl: './playlist-list.component.html',
   styleUrl: './playlist-list.component.scss',
 })
 export class PlaylistListComponent {
-  playlists = [
-    {
-      id: '123',
-      name: 'Playlist 123',
-      public: true,
-      description: 'Awesome playlsit',
-    },
-    {
-      id: '234',
-      name: 'Playlist 234',
-      public: false,
-      description: 'Best playlsit',
-    },
-    {
-      id: '345',
-      name: 'Playlist 345',
-      public: true,
-      description: 'Cool playlsit',
-    },
-  ];
+  playlists = mockPlaylists;
+
+  selectedId = '234';
+
+  select(id: string) {
+    this.selectedId = id;
+  }
 
   byId(index: number, p: { id: string }) {
     return p.id;
