0b19b30c37a10d5cce42db1074c557feaf89f8bf
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-11 09:00:31 +0200

    { provide: NG_VALUE_ACCESSOR, useExisting: ContenteditableDirective, multi:true }

--- src/app/shared/directives/contenteditable.directive.ts
+++ src/app/shared/directives/contenteditable.directive.ts
@@ -1,10 +1,25 @@
 import { Directive } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Directive({
-  selector: '[contenteditable][ngModel]'
+  selector: '[contenteditable][ngModel]',
+  providers: [
+    { provide: NG_VALUE_ACCESSOR, useExisting: ContenteditableDirective, multi:true },
+  ],
 })
-export class ContenteditableDirective {
+export class ContenteditableDirective implements ControlValueAccessor {
+  constructor() {}
 
-  constructor() { }
-
+  writeValue(obj: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnChange(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
 }
