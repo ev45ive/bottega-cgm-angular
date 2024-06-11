530d929c938d8190fcc6d9115d7ea7d6f345fe7e
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-11 08:44:44 +0200

    Example test

--- src/app/playlists/components/playlist-editor/playlist-editor.component.spec.ts
+++ src/app/playlists/components/playlist-editor/playlist-editor.component.spec.ts
@@ -1,20 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlaylistEditorComponent } from './playlist-editor.component';
+import { By } from '@angular/platform-browser';
 
-describe('PlaylistEditorComponent', () => {
+fdescribe('PlaylistEditorComponent', () => {
   let component: PlaylistEditorComponent;
   let fixture: ComponentFixture<PlaylistEditorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PlaylistEditorComponent]
-    })
-    .compileComponents();
+      declarations: [PlaylistEditorComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PlaylistEditorComponent);
+    // fixture.changeDetectorRef
+    // fixture.componentInstance
+    // fixture.debugElement
+    // fixture.nativeElement
+    // fixture.ngZone
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+  
+  it('should update model', () => {
+    const playlist_name = fixture.debugElement.query(By.css('#playlist_name'));
+    playlist_name.nativeElement.value = 'placki';
+    playlist_name.nativeElement.dispatchEvent(new InputEvent('input'));
+    fixture.detectChanges();
+    expect(component.playlist.name).toEqual('placki 123')
   });
 
   it('should create', () => {
