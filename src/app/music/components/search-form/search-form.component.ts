d6cf5fd4744ce57edcdfdfbb9d19232cc672c4fe
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-12 11:41:53 +0200

    Reactive Form Validators

--- src/app/music/components/search-form/search-form.component.ts
+++ src/app/music/components/search-form/search-form.component.ts
@@ -1,12 +1,23 @@
 import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { MaterialImports, SharedModule } from '../../../shared/shared.module';
 import {
+  AbstractControl,
   FormArray,
   FormBuilder,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
 } from '@angular/forms';
+
+const censor =
+  (badword: string) =>
+  (control: AbstractControl<any, any>): ValidationErrors | null => {
+    return String(control.value).includes(badword)
+      ? { censor: { badword: badword } }
+      : null;
+  };
 
 @Component({
   selector: 'app-search-form',
@@ -23,7 +34,11 @@ export class SearchFormComponent {
   bob = inject(FormBuilder);
 
   searchForm = this.bob.group({
-    query: ['batman'],
+    query: ['batman',[
+      Validators.required,
+      Validators.minLength(3),
+      censor('batman')
+    ]],
     advanced: this.bob.group({
       type: ['album'],
       markets: this.bob.array([
@@ -48,17 +63,21 @@ export class SearchFormComponent {
     );
   }
 
-  // searchForm2 = new FormGroup({
-  //   query: new FormControl('batman'),
-  //   advanced: new FormGroup({
-  //     type: new FormControl('album'),
-  //     markets: this.bob.array([
-  //       new FormGroup({
-  //         code: new FormControl('PL'),
-  //       }),
-  //     ]),
-  //   }),
-  // });
+  searchForm2 = new FormGroup({
+    query: new FormControl('batman', [
+      Validators.required,
+      Validators.minLength(3),
+      censor('batman')
+    ]),
+    advanced: new FormGroup({
+      type: new FormControl('album'),
+      markets: this.bob.array([
+        new FormGroup({
+          code: new FormControl('PL'),
+        }),
+      ]),
+    }),
+  });
 
   submit() {
     this.search.emit(this.searchForm.value.query || '');
