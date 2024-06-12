9af92dd080d8552b5b4b9691fb88f9bb4e21c33a
Mateusz Kulesza <ev45ive@gmail.com>
2024-06-12 10:44:17 +0200

    catchError((error: unknown... , if (!(error instanceof ... , originalObservable, // retry ,EMPTY  NEVER

--- src/app/core/services/music-api.service.ts
+++ src/app/core/services/music-api.service.ts
@@ -1,10 +1,10 @@
 import { Injectable, inject } from '@angular/core';
 import { API_URL } from '../../tokens';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { AlbumSearchResponse } from './model/album';
 import { AuthService } from './auth.service';
 import { mockAlbums } from '../fixtures/mockAlbums';
-import { map, of } from 'rxjs';
+import { EMPTY, NEVER, catchError, from, map, of, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -18,19 +18,35 @@ export class MusicApiService {
     const token = this.auth.getToken();
 
     // Unicast Observable // 1-1 // Recipe
-    return this.http.get<AlbumSearchResponse>(this.api_url + 'search', {
-      headers: {
-        Authorization: 'Bearer ' + token,
-      },
-      params: {
-        type: 'album',
-        q: query,
-      },
-    }).pipe(
-      map(res => res.albums.items)
-    )
+    return this.http
+      .get<AlbumSearchResponse>(this.api_url + 'search', {
+        headers: {
+          Authorization: 'Bearer ' + token,
+        },
+        params: {
+          type: 'album',
+          q: query,
+        },
+      })
+      .pipe(
+        map((res) => res.albums.items),
+        catchError((error: unknown, originalObservable) => {
 
-    // return of(mockAlbums)
+          if (!(error instanceof HttpErrorResponse))
+            throw new Error('Unexpected error');
 
+          throw new Error(error.error.error.message);
+
+          // return originalObservable // retry
+          // return this.http.get(inny server).pipe( itd ..)
+          // return throwError(() => new Error(error.error.error.message))
+          // return from([mockAlbums])
+          // return of(mockAlbums)
+          // return [mockAlbums, mockAlbums] // OO|>
+          // return []; // ---|>
+          // return EMPTY // --|>
+          // return NEVER
+        }),
+      );
   }
 }
--- src/app/music/containers/album-search-view/album-search-view.component.ts
+++ src/app/music/containers/album-search-view/album-search-view.component.ts
@@ -1,30 +1,29 @@
 import { Component } from '@angular/core';
-import { SearchFormComponent } from "../../components/search-form/search-form.component";
-import { ResultsGridComponent } from "../../components/results-grid/results-grid.component";
+import { SearchFormComponent } from '../../components/search-form/search-form.component';
+import { ResultsGridComponent } from '../../components/results-grid/results-grid.component';
 import { mockAlbums } from '../../../core/fixtures/mockAlbums';
 import { MusicApiService } from '../../../core/services/music-api.service';
 import { Album } from '../../../core/services/model/album';
 
 @Component({
-    selector: 'app-album-search-view',
-    standalone: true,
-    templateUrl: './album-search-view.component.html',
-    styleUrl: './album-search-view.component.scss',
-    imports: [SearchFormComponent, ResultsGridComponent]
+  selector: 'app-album-search-view',
+  standalone: true,
+  templateUrl: './album-search-view.component.html',
+  styleUrl: './album-search-view.component.scss',
+  imports: [SearchFormComponent, ResultsGridComponent],
 })
 export class AlbumSearchViewComponent {
-    results:Album[] = []
-    message = ''
+  results: Album[] = [];
+  message = '';
 
-    constructor(private api: MusicApiService){}
-    
-    searchAlbums(query: string) {
-       this.api
-        .fetchAlbums(query) 
-        .subscribe({
-            next: (albums) => this.results = albums,   // --O--O--O--O--O->
-            error: (error) => this.message = error.error.error.message, // --------X>
-            // complete: () => console.log('complete'),                 // ----O------O---|>
-        })
-    }
+  constructor(private api: MusicApiService) {}
+
+  searchAlbums(query: string) {
+    this.api
+    .fetchAlbums(query)
+    .subscribe({
+      next: (albums) => (this.results = albums), // --O--O--O--O--O->
+      error: (error) => (this.message = error.message), // --------X>
+    });
+  }
 }
